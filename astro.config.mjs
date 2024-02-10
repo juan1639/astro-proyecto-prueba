5af796981412028ff0be2f11a9812a15ba28f3eb
unknown <juaneguiabad1639@gmail.com>
2024-02-10 13:21:13 +0100

    agregado pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  base: 'https://juan1639.github.io/astro-proyecto-prueba'
 });
